e3625739a546d3d52aa859cde120a73745750d40
MD Masum Rana <masummx3@gmail.com>
2023-02-08 20:13:51 +0600

    db connect

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 
 const app = require("./app");
+const dbConnect = require("./utils/dbConnect");
 
 // const url=
 
 //database connection
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yofznvf.mongodb.net/?retryWrites=true&w=majority`).then(() => {
-  console.log(`Succeeded to connect the database`.red.bold);
-});
+dbConnect()
 
 //server
 const port = process.env.PORT || 8080;
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,27 +1,7 @@
-const { MongoClient } = require("mongodb");
-const connectionString = 'mongodb://scholarships_australia:wGGBhTLq7XS3cUHT@main-shard-00-00-03xkr.mongodb.net:27017,main-shard-00-01-03xkr.mongodb.net:27017,main-shard-00-02-03xkr.mongodb.net:27017/main?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true';
-const client = new MongoClient(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const mongoose = require('mongoose')
+function dbConnect(){
+  mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yofznvf.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+  console.log(`Succeeded to connect the database`.red.bold);
 });
-console.log(connectionString);
-
-let dbConnection;
-
-
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      if (err || !db) {
-        return callback(err);
-      }
-      dbConnection = db.db("scholarships_australia");
-      console.log("Successfully connected to MongoDB.");
-      return callback();
-    });
-  },
-
-  getDb: function () {
-    return dbConnection;
-  },
-};
+}
+module.exports=dbConnect;
