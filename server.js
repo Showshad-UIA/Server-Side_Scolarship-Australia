6fa0e0add8014d14c7f44e2f993ce4dbf3a74f53
raihan <raihanulmcse@mail.com>
2023-02-08 16:24:43 +0800

    server connected

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv").config();
-const colors = require("colors");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
 
-const app = require("./app");
+const app = require('./app');
 
 // const url=
 
 //database connection
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yofznvf.mongodb.net/?retryWrites=true&w=majority`).then(() => {
-  console.log(`Succeeded to connect the database`.red.bold);
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yofznvf.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log(`Succeeded to connect the database`.red.bold);
+  });
 
 //server
 const port = process.env.PORT || 8080;
@@ -18,4 +22,3 @@ const port = process.env.PORT || 8080;
 //     console.log(port)
 //     console.log(`App is running on prot ${port}`.yellow.bold);
 //   });
-
